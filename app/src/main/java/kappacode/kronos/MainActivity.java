556207ca60ca6242e20d640735ffe7bef036fb2f
gjmbarbosa <gjmbarbosa@outlook.com>
2016-10-19 01:27:14 -0200

    Initial commit

---
+++ app/src/main/java/kappacode/kronos/MainActivity.java
@@ -0,0 +1,37 @@
+package kappacode.kronos;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import kappacode.kronos.fragments.*;
+import kappacode.kronos.viewpageradapter.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Goals(), "Goals");
+        adapter.addFragment(new MyDay(), "My Day");
+        adapter.addFragment(new History(), "History");
+        viewPager.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/kappacode/kronos/fragments/MyDay.java
@@ -0,0 +1,28 @@
+package kappacode.kronos.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kappacode.kronos.R;
+
+public class MyDay extends Fragment{
+
+    public MyDay() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.myday, container, false);
+    }
+
+}
