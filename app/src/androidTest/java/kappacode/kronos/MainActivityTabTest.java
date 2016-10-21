6bff3320630619b2e3c80ab99ae1bf15b32f658a
gjmbarbosa <gjmbarbosa@outlook.com>
2016-10-20 23:46:12 -0200

    test update 3

--- app/src/androidTest/java/kappacode/kronos/MainActivityTabTest.java
+++ app/src/androidTest/java/kappacode/kronos/MainActivityTabTest.java
@@ -4,7 +4,6 @@ package kappacode.kronos;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -22,7 +21,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.allOf;
 
-@LargeTest
+
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTabTest {
 
