504a279574df941e5d07ed36ce7535a725a280cc
gjmbarbosa <gjmbarbosa@outlook.com>
2016-10-24 21:06:44 -0200

    add button add new activity

--- app/src/main/java/kappacode/kronos/fragments/MyDay.java
+++ app/src/main/java/kappacode/kronos/fragments/MyDay.java
@@ -5,24 +5,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
 import kappacode.kronos.R;
 
 public class MyDay extends Fragment{
+    private ListView listViewActivities;
 
     public MyDay() {
         // Required empty public constructor
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.myday, container, false);
+
+        View inflaterView = inflater.inflate(R.layout.myday, container, false);
+
+        View temp =inflaterView.findViewById(R.id.view0);
+
+        listViewActivities =(ListView) inflaterView.findViewById(R.id.listView_activities);
+
+        Button buttonAddActivity = (Button) inflaterView.findViewById(R.id.button_add_activity);
+        buttonAddActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // do something
+            }
+        });
+
+        return inflaterView;
     }
 
 }
--- app/src/main/java/kappacode/kronos/viewpageradapter/ViewPagerAdapter.java
+++ app/src/main/java/kappacode/kronos/viewpageradapter/ViewPagerAdapter.java
@@ -22,6 +22,7 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
+
         return mFragmentList.size();
     }
 
