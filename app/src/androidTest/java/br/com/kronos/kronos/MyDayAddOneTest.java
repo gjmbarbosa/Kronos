2f8f8e30afbaf962be349b791ff094d1a775ea28
gjmbarbosa <gjmbarbosa@outlook.com>
2016-11-23 23:56:19 -0200

    update

--- app/src/androidTest/java/br/com/kronos/kronos/MyDayAddOneTest.java
+++ app/src/androidTest/java/br/com/kronos/kronos/MyDayAddOneTest.java
@@ -5,8 +5,14 @@ import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.view.WindowManager;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,7 +21,6 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -23,7 +28,7 @@ import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class MainActivityClickTest {
+public class MyDayAddOneTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
@@ -43,40 +48,41 @@ public class MainActivityClickTest {
 
 
     @Test
-    public void mainActivityClickTest() {
+    public void myDayAddOneTest() {
         ViewInteraction appCompatButton = onView(
                 allOf(withId(R.id.button_myDay), withText("My Day"), isDisplayed()));
         appCompatButton.perform(click());
 
-        ViewInteraction imageButton = onView(
-                allOf(withContentDescription("Navigate up"),
-                        withParent(allOf(withId(R.id.action_bar),
-                                withParent(withId(R.id.action_bar_container)))),
-                        isDisplayed()));
-        imageButton.perform(click());
-
         ViewInteraction appCompatButton2 = onView(
-                allOf(withId(R.id.button_history), withText("History"), isDisplayed()));
+                allOf(withId(R.id.button_adicionar_atividdade), withText("+ Add a new Activity"), isDisplayed()));
         appCompatButton2.perform(click());
 
-        ViewInteraction imageButton2 = onView(
-                allOf(withContentDescription("Navigate up"),
-                        withParent(allOf(withId(R.id.action_bar),
-                                withParent(withId(R.id.action_bar_container)))),
+        ViewInteraction appCompatCheckBox = onView(
+                allOf(withId(R.id.checkBox_inserir_grafico),
+                        withParent(childAtPosition(
+                                withId(R.id.listView_atividades),
+                                1)),
                         isDisplayed()));
-        imageButton2.perform(click());
-
-        ViewInteraction appCompatButton3 = onView(
-                allOf(withId(R.id.button_goal), withText("Goal"), isDisplayed()));
-        appCompatButton3.perform(click());
-
-        ViewInteraction imageButton3 = onView(
-                allOf(withContentDescription("Navigate up"),
-                        withParent(allOf(withId(R.id.action_bar),
-                                withParent(withId(R.id.action_bar_container)))),
-                        isDisplayed()));
-        imageButton3.perform(click());
+        appCompatCheckBox.perform(click());
 
     }
 
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
 }
